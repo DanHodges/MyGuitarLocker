7fdaf44a62ebb3718138e6a616d1aa87e8686ed2
DanHodges <dhdgs690@gmail.com>
2015-12-28 10:48:53 -0600

    final wilder commit with babel

--- src/Wildermuth/wwwroot/js/tripEditorController.js
+++ src/Wildermuth/wwwroot/js/tripEditorController.js
@@ -10,15 +10,57 @@
         vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+        vm.newStop = {};
+        
+        var url = "/api/trips/" + vm.tripName + "/stops";
 
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        $http.get(url)
           .then(function (response) {
               angular.copy(response.data, vm.stops);
-          }, function (err) {
+              _showMap(vm.stops);
+              console.log("_showMap(vm.stops)")
+;          }, function (err) {
               vm.errorMessage = "Failed to load stops";
           })
           .finally(function () {
               vm.isBusy = false;
           });
+
+        vm.addStop = function () {
+            vm.isBusy = true;
+            $http.post(url, vm.newStop)
+                .then(function (response) {
+                    //success
+                    vm.stops.push(response.data);
+                    _showMap(vm.stops);
+                    vm.newStop = {};
+                }, function () {
+                    //error
+                    vm.errorMessage = "Failed to save new stop";
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
+        }
+    }
+
+    function _showMap(stops) {
+        if (stops && stops.length > 0) {
+            var mapStops = _.map(stops, function(item){
+                return {
+                    lat : item.latitude,
+                    long: item.longitude,
+                    info : item.name
+                };
+            });
+            //show map
+            console.log("_showMap()");
+            travelMap.createMap({
+                stops: mapStops,
+                selector: "#map",
+                currentStop: 1,
+                initialZoom: 3
+            });
+        }
     }
 })();
